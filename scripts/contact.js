f6eebdeba36a33afd661429a5868bb018d988240
Aleks <nikolic.aleks90@gmail.com>
2020-05-15 14:08:03 -0400

    form submit functioning correctly

---
+++ scripts/contact.js
@@ -0,0 +1,54 @@
+// Using Formspree for simple form submission
+
+window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+    
+    const form = document.getElementById("contactForm");
+    const button = document.getElementById("submit");
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+        form.reset();
+        status.innerHTML = "Thanks!";
+        $('#contactForm').prepend(`<div class='emailSent'><p>Your message successfully submitted. Thank you, I will get back to you soon!</p><button id='closePopup' onClick='removeDummy()'><i class="fas fa-times"></i></button></div>`)
+    }
+
+    function error() {
+        status.innerHTML = "Oops! There was a problem.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        let data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
+});
+
+  // helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
+
+// Close successful message popup
+
+function removeDummy() {
+    var elem = document.getElementById('closePopup');
+    remove(elem);
+    return false;
+}
