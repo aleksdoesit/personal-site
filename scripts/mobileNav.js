f262f0ed887fb1fb7be0ff9af08c838c47351e33
Aleks <nikolic.aleks90@gmail.com>
2020-05-18 14:52:18 -0400

    Added collapsing side nav functionality and smoothscrolling

--- scripts/mobileNav.js
+++ scripts/mobileNav.js
@@ -1,7 +1,37 @@
 // Simple slide in function with css animations for the mobile menu
 
 $('#toggleMenu').on('click', function() {
-    $('#fullHeader').toggleClass('headerToggle')
-    $('.wrapper').toggleClass('toggleBody')
-    $('.parallaxBackground').toggleClass('toggleBody')
+    $('#fullHeader').toggleClass('headerToggle');
+    $('.wrapper').toggleClass('toggleBody');
+    $('.parallaxBackground').toggleClass('toggleBody');
 })
+
+// Smooth scrolling to sections by href
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+
+    // Get element's href/section id
+    let elementHref = $(this).attr('href');
+
+    // Declare target variable to be id from stored href
+    let target = $(elementHref);
+
+    if (target.length === 0) {
+        return;
+    }
+
+    event.preventDefault();
+
+    // Get the top position of the selected section
+    let pos = target.offset().top;
+
+    // Scroll to this new target top position
+    $('body, html').animate({
+        scrollTop: pos
+    });
+
+    // Collapse the side nav menu once clicked
+    $('#fullHeader').toggleClass('headerToggle');
+    $('.wrapper').toggleClass('toggleBody');
+    $('.parallaxBackground').toggleClass('toggleBody');
+});
