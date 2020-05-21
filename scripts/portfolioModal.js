44e3a1ece197a865fd0a7d0b82ec355362de9fe9
Aleks <nikolic.aleks90@gmail.com>
2020-05-21 17:32:45 -0400

    some small changes for chrome on the modal popup

--- scripts/portfolioModal.js
+++ scripts/portfolioModal.js
@@ -14,8 +14,8 @@ document.getElementById('portfolioItem1').addEventListener('click', function () 
     <img class='portfolioImage1'src='./assets/robotrivia.png' alt='an image of my app RoboTrivia'>
     <h3>RoboTrivia!</h3>
     <div>
-    <p>A collaborative project using mob programming to deliver a dynamic multiplayer trivia application. Designed and deployed under a strict turn-around time with the use of a GitHub and command line workflow</p>
-    <p>This project utilised React and dynamic API calls to generate questions from an array of categories. In addition scores are tracked and uploaded to a leaderboard hosted on Firebase</p>
+    <p>A collaborative project using mob programming to deliver a dynamic multiplayer trivia application. Designed and deployed under a strict turn-around time with the use of a GitHub and command line workflow.</p>
+    <p>This project utilised React and dynamic API calls to generate questions from an array of categories. In addition scores are tracked and uploaded to a leaderboard hosted on Firebase.</p>
     </div>
     <div>
     <button><a href='https://robogames.github.io/RoboTrivia' target="_blank"/>Visit Site</a></button><button><a href='https://github.com/RoboGames/RoboTrivia' target="_blank">Git Hub</a></button>
