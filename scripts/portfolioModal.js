c47b16687b71de854ba9d827a481438631feba0e
Aleks <nikolic.aleks90@gmail.com>
2020-05-21 17:34:56 -0400

    small changes for overflow on the modal popout for chrome browser

--- scripts/portfolioModal.js
+++ scripts/portfolioModal.js
@@ -17,7 +17,7 @@ document.getElementById('portfolioItem1').addEventListener('click', function () 
     <p>A collaborative project using mob programming to deliver a dynamic multiplayer trivia application. Designed and deployed under a strict turn-around time with the use of a GitHub and command line workflow.</p>
     <p>This project utilised React and dynamic API calls to generate questions from an array of categories. In addition scores are tracked and uploaded to a leaderboard hosted on Firebase.</p>
     </div>
-    <div>
+    <div class='linkWrapper'>
     <button><a href='https://robogames.github.io/RoboTrivia' target="_blank"/>Visit Site</a></button><button><a href='https://github.com/RoboGames/RoboTrivia' target="_blank">Git Hub</a></button>
     </div>
     </div>
