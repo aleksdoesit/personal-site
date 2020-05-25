d3b54b6d6ace31a692fd73b2d296aa4155f326fd
Aleks <nikolic.aleks90@gmail.com>
2020-05-25 18:59:20 -0400

    added second project link and accompanying modal, woo

--- scripts/portfolioModal.js
+++ scripts/portfolioModal.js
@@ -1,3 +1,5 @@
+// Modal for Item 1
+
 document.getElementById('portfolioItem1').addEventListener('click', function () {
 
     let portfolioDiv = document.createElement('div')
@@ -19,6 +21,43 @@ document.getElementById('portfolioItem1').addEventListener('click', function () 
     </div>
     <div class='linkWrapper'>
     <button><a href='https://robogames.github.io/RoboTrivia' target="_blank"/>Visit Site</a></button><button><a href='https://github.com/RoboGames/RoboTrivia' target="_blank">Git Hub</a></button>
+    </div>
+    </div>
+    `
+
+    document.getElementById('portfolio').appendChild(portfolioDiv)
+    document.getElementById('closePortfolioItem').addEventListener('click', function () {
+        document.getElementById('portfolioDiv').remove()
+    })
+
+    document.getElementById('portfolioMask').addEventListener('click', function () {
+        document.getElementById('portfolioDiv').remove()
+    })
+})
+
+// Modal for Item 2
+
+document.getElementById('portfolioItem2').addEventListener('click', function () {
+
+    let portfolioDiv = document.createElement('div')
+
+    portfolioDiv.classList.add('portfolioDiv')
+
+    portfolioDiv.id = 'portfolioDiv'
+
+    portfolioDiv.innerHTML = `
+    <div id='portfolioMask' class='portfolioMask'>
+    <button id='closePortfolioItem' class='closePortfolioItem'><i class="fas fa-times"></i></button>
+    </div>
+    <div class='popupContainer'>
+    <img class='portfolioImage1'src='./assets/bingr.png' alt='an image of my app Bingr'>
+    <h3>Bingr</h3>
+    <div>
+    <p>A passion project application designed to be a one-stop place that currates TV show recommendations across all networks and platforms. Making use of React, React Router, and RESTful API calls to build a simple, snappy, and quick one-page React web app.</p>
+    <p>Please note this is a work in progress! Responsiveness not yet fully functional :)</p>
+    </div>
+    <div class='linkWrapper'>
+    <button><a href='https://aleksdoesit.github.io/Bingr/#/' target="_blank"/>Visit Site</a></button><button><a href='https://github.com/aleksdoesit/Bingr' target="_blank">Git Hub</a></button>
     </div>
     </div>
     `
